eb78845f734dbcc55f06da1dc1d6c66c12cef182
Hadi <test@test.com>
2022-01-25 16:39:02 +0500

    vuexpagination

---
+++ src/views/User.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- <h2 class="user">User {{ $route.params.username }} component</h2> -->
+  <h2>User {{ username }} component</h2>
+</template>
+<script>
+export default {
+  props: ["username"]
+}
+</script>
